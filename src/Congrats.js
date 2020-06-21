76058c2a28c1704d66b1c1cbd630c1935de7e05b
pachinko <sunpochin@gmail.com>
2020-06-21 23:07:11 +0800

    aadding setup.

---
+++ src/Congrats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory
+ */
+
+export default() => {
+    return (
+        <div></div>
+    )
+}
---
+++ src/Congrats.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
+
+import { findByTestAttr } from '../test/testUtils';
+import Congrats from './Congrats';
+
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
+
+
+const setup = (props={}) => {
+    // props = {success: true};
+    // console.log('props: ', props)
+    // return shallow(<Congrats {...props}/>);
+
+    return shallow(<Congrats {...props}/>);
+}
+
+test('renders without error', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.length).toBe(1);
+});
+
+
+test('renders no text when `success` prop is false', () => {
+    const wrapper = setup({ success: false} );
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text() ).toBe('');
+
+});
+
+
+test('renders non empty congrats message when `success` prop is true', () => {
+
+});
+
