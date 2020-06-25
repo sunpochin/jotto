098a91e5cb88d67acca0bf33c6c52bf3041b88cd
pachinko <sunpochin@gmail.com>
2020-06-25 23:23:31 +0800

    aadding congrats implement to fix failing tests.

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 /**
- * Functional react component for congratulatory
+ * Functional react component for congratulatory message
+ * @function 
+ * @param {object} props - React props
+ * @returns {JSX.element} - Rendered component, or null if `success` prop if false.
  */
 
-export default() => {
-    return (
-        <div></div>
-    )
+// const Congrats = (props) => {
+export default(props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="component-message">
+                    Congratulations! You guessed the WORD!
+                </span>
+            </div>
+        );
+    } else {
+        return (
+            <div data-test="component-congrats"/>
+        );
+    }
 }
+
+// Congrats.propTypes = {
+//     success: Proptypes.bool.isRequired, 
+// };
+
+
+// export default Congrats;
+
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -27,11 +27,12 @@ test('renders no text when `success` prop is false', () => {
     const wrapper = setup({ success: false} );
     const component = findByTestAttr(wrapper, 'component-congrats');
     expect(component.text() ).toBe('');
-
 });
 
 
 test('renders non empty congrats message when `success` prop is true', () => {
-
+    const wrapper = setup({ success: true} );
+    const message = findByTestAttr(wrapper, 'component-message');
+    expect(message.text().length ).not.toBe(0);
 });
 
