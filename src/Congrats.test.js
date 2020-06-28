9da2e539e0392caa7af730becd539a7c78e2e6e9
pachinko <sunpochin@gmail.com>
2020-06-29 02:51:10 +0800

    extract Enzyme setup to setupTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
-import EnzymeAdaptor from 'enzyme-adapter-react-16';
+// import EnzymeAdaptor from 'enzyme-adapter-react-16';
 
 import checkPropTypes from 'check-prop-types';
 
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdaptor() });
+// Enzyme.configure({ adapter: new EnzymeAdaptor() });
 
 
 const defaultProps = {success: false};
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
